ffd5e46b285ef598e92437f725b7d733c0a8c575
SaraDurann <saradurann@gmail.com>
2019-09-13 00:19:00 -0500

    otro mas

--- app/src/main/java/org/sara/test1/test1activity.java
+++ app/src/main/java/org/sara/test1/test1activity.java
@@ -9,5 +9,9 @@ public class test1activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test1activity);
+
+        //ksdlngflsdngf
+        //kajdsbfjlsedf
+        
     }
 }
